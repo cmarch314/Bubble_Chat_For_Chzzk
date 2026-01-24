c51ef1fb11600eec4ae8042cf22d510b8293a23f
HoChoi <cmarch314@gmail.com>
2026-01-23 17:47:20 -0800

    Emergency Recovery: Restored stable version 1.3.1 from verified backup directory.

--- config.js
+++ config.js
@@ -1,39 +1,35 @@
-﻿// [Config Logic] Centralized Configuration for BubbleChat
+// [Config Logic] Centralized Configuration for BubbleChat
 // You can manually edit this file or overwrite it using the Export function in config.html
 
 window.HIVE_SOUND_CONFIG = {
+    // Screen Effects
     "해골": "SkullMeme.mp3",
     "커플": "DieWithASmile.mp3",
-    "아하하": "laugh3.mp3",
-    "ㅋㅋㅋㅋㅋ": "laugh.mp3",
-    "ㅋㅋㅋㅋㅋㅋ": "laugh4.mp3",
-    "안되": "No.mp3",
-    "홋치": "whip.mp3",
-    "오옷": "Ot.wav",
-    "헤으응": "careless.mp3",
+
+    // [Auto-Discovered SFX]
+    "전화": "Call.wav",
+    "헤으응": "Careless.mp3",
     "뚝": "DDuk.wav",
-    "갓겜": "godgame.mp3",
-    "멈춰": "Stop.mp3",
-    "스트레스": "StressNeeds.mp3",
-    "운동": "workouthard.mp3",
-    "배운다": "Learing.mp3",
-    "야스": "Yas.mp3",
+    "커플곡": "DieWithASmile.mp3",
     "잠시후": "FUmomentsLater.mp3",
-    "와우": "Wow.mp3",
-    "디코": "discord.mp3",
-    "카톡": "Katalk.wav",
-    "전화": "Call.WAV",
-    "보이스콜": "KakaoCall.mp3",
-    "!유격": "MohyungTower.mp3",
-    "안돼": "no (1).mp3",
     "폭풍": "I AM THE STORM.mp3",
-    "결제": "applepay.mp3",
-    "윽": "ast5.mp3",
-    "꺼져": "foff.mp3"
+    "보이스콜": "KakaoCall.mp3",
+    "카톡": "Katalk.wav",
+    "아하하": "Laugh3.mp3",
+    "ㅋㅋㅋㅋㅋㅋ": "Laugh4.mp3",
+    "배운다": "Learing.mp3",
+    "유격": "MohyungTower.mp3",
+    "안돼": "no (1).mp3",
+    "안되": "No.mp3",
+    "아냐": "no (1).mp3",
+    "쇼크": "shocked-sound-effect.mp3",
+    "해골밈": "skullmeme.mp3",
+    "운동": "workouthard.mp3",
+    "홋치": "whip.mp3",
+    "앙": "ahng.mp3"
 };
 
 window.HIVE_VISUAL_CONFIG = {
     "해골": "skull",
-    "커플": "heart",
-    "폭풍": "slash"
+    "커플": "heart"
 };
